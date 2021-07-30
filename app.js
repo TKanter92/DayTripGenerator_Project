986d4b15d787b11934e392f6d66d29e3d3dc6c3d
TKanter92 <TylerKanter@yahoo.com>
2021-07-30 17:02:14 -0500

    changed the layout of functions to try and accomodate for changes made by user.

--- app.js
+++ app.js
@@ -7,61 +7,51 @@ function runDayTripGenerator(){
     let transOptions = ["Plane", "Bus", "Car", "Train", "Boat", "Police Escort", "Presidential Escort"];
     let funOptions = ["Comedy Show", "Waterpark", "Beach", "Museum", "Sporting Event"];
 
-    
-
     let location = locationGenerator(destOptions);
-    let foodChoice = foodGenerator(foodOptions, location);
-    transGenerator(transOptions);
-    entGenerator(funOptions);
+    let foodChoice = foodGenerator(foodOptions);
+    let transportation = transGenerator(transOptions);
+    let entertainment = entGenerator(funOptions);
 
-    console.log()
+    console.log("This is the current trip : You will be heading to " + location + " via " + transportation + ". You will stop for food at " + foodChoice + ", and spend some time at a " + entertainment + "!");
 
-    let userInput = prompt("What would you like to change: 1. destination 2. ")
+    let userInput = prompt("Please enter the number associated with the detail you wish to change. 1. Destination 2. Transportation 3. Food 4. Entertainment 5. No Change Needed");
 
     if(userInput == "1"){
-        location = locationGenerator(destOptions);
+        locationGenerator(destOptions);
     }
-    else if()
+    else if(userInput == "2"){
+        transportation = transGenerator(transOptions);
+    }
+    else if (userInput == "3"){
+        foodChoice = foodGenerator(foodOptions);
+    }
+    else if (userInput == "4"){
+        entertainment = entGenerator(funOptions);
+    }
+    else if (userInput == "5"){
+        console.log("Great choice! Enjoy your Trip! Your day trip generator is now complete.")
+    }
 }
+
 
 function locationGenerator(randomTrip){
-    let number = Math.floor((Math.random() * randomTrip.length));
-    return randomTrip[number]
+    let place = Math.floor((Math.random() * randomTrip.length));
+    return randomTrip[place]
 }
 
-
-
-function foodGenerator(foodChoices, location){
-    let foodChoice = false;
-    for (let i = 0; i < foodChoices.length; i++){
-        let restChoice = prompt("Would you like to eat at " + foodChoices[i] + " while in " + location + "? Please Enter yes or no.");
-        if (foodChoice !== restChoice)
-        foodChoices[i] ++;
-        if (foodChoice === restChoice)
-        return foodChoice;
-    }
+function foodGenerator(foodChoices){
+    let foods = Math.floor((Math.random() * foodChoices.length));
+    return foodChoices[foods]
 }
 
-// function transGenerator(transChoices){
-//     let transChoice = false;
-//     for (let i = 0; i < transChoices.length; i++){
-//         let carChoice = prompt("Would you like to travel to " + location + " by " + transChoices[i] + "? Please Enter yes or no.");
-//         if (transChoice !== carChoice)
-//         transChoices[i] ++;
-//         if (transChoice === carChoice)
-//         return transChoice;
-//     }
-// }
+function transGenerator(transChoices){
+    let vehicle = Math.floor((Math.random() * transChoices.length));
+    return transChoices[vehicle]
+}
 
-// function entGenerator(funChoices){
-//     let funChoice = false;
-//     for (let i = 0; i < funChoices.length; i++){
-//         let entertainment = prompt("Would you like to go to a " + funChoices[i] + " while in " + location + "? Please Enter yes or no.");
-//         if (entertainment !== funChoice)
-//         funChoices[i] ++;
-//         if (entertainment === funChoice)
-//         return funChoice;
-//     }
-// }
+function entGenerator(funChoices){
+    let fun = Math.floor((Math.random() * funChoices.length));
+    return funChoices[fun]
+}
 
 runDayTripGenerator();
